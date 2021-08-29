069618bae90e7af19da0f81bc1df1fc3f9a0472b
Gustavo Lima <gustavomarques.gml@gmail.com>
2021-08-29 12:41:20 -0300

    Addition of production settings

--- api/task/views.py
+++ api/task/views.py
@@ -8,17 +8,21 @@ class TaskCreate(generics.CreateAPIView):
     queryset = Task.objects.all(),
     serializer_class = TaskSerializer
 
+
 class TaskList(generics.ListAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
 
 class TaskDetail(generics.RetrieveAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
+
 class TaskUpdate(generics.RetrieveUpdateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
 
 class TaskDelete(generics.RetrieveDestroyAPIView):
     queryset = Task.objects.all()
