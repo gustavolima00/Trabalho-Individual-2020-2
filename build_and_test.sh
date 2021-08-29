a96d24f10a96274de611216a5dcc8ee4fc114525
Gustavo Lima <gustavomarques.gml@gmail.com>
2021-08-29 17:15:01 -0300

    Update of build and test script

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,4 +1,4 @@
-docker-compose -f api/docker-compose.yml up --build -d || exit 1
+docker-compose -f api/docker-compose.prod.yml up --build -d || exit 1
 docker-compose -f client/docker-compose.yml up --build -d || exit 1
-docker-compose -f api/docker-compose.yml exec web python3 manage.py test || exit 1
-sudo docker-compose -f client/docker-compose.yml exec client quasar test --unit jest || exit 1
+docker-compose -f api/docker-compose.prod.yml exec web python manage.py test || exit 1
+docker-compose -f client/docker-compose.yml exec client quasar test --unit jest || exit 1
